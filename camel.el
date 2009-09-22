70069eee8acdf4d50fec50314b2329dde3c6fab3
David O'Toole <dto@gnu.org>
2009-09-22 05:26:18 -0400

    following.

--- camel.el
+++ camel.el
@@ -1,4 +1,4 @@
-;;; camel.el --- configurable support for CamelCase links in any buffer
+;; camel.el --- configurable support for clickable links in any buffer
 
 ;; Copyright (C) 2009  David O'Toole
 
@@ -20,15 +20,12 @@
 
 ;;; Commentary:
 
-;; This program adds support for CamelCase words that can be used as
-;; Wiki links in Emacs buffers.
+;; This program adds support for clickable CamelCase words that can be
+;; used as Wiki links in Emacs buffers.
 
 ;; For discussion of the issues surrounding CamelCase words, see:
 ;; http://en.wikipedia.org/wiki/CamelCase
 ;; and also http://www.c2.com/cgi/wiki/wiki?CamelCase
-
-;; TODO click to open
-;; TODO right-click context menu
 
 ;;; Code:
 
@@ -37,7 +34,7 @@
 (defvar *camel-link-handler* nil
   "Function to handle links.")
 
-(defun camel-install-link-handler (handler)
+(defun camel-set-link-handler (handler)
   (assert (functionp handler))
   (setf *camel-link-handler* handler))
 
@@ -52,11 +49,25 @@
 	       (string-match *camel-regexp* word))
       (camel-handle-link word))))
 
+(defun camel-mouse-follow-link (event)
+  (interactive "e")
+  (let ((pos (posn-point (event-end event)))
+	(window (posn-window (event-end event))))
+    (set-buffer (window-buffer window))
+    (goto-char pos)
+    (camel-follow-link-at-point)))
+      
 (defface camel-face
 '((t (:foreground "cyan" :underline "yellow")))
   "Face for CamelCase links.")
 
 (defvar camel-face 'camel-face)
+
+(defface camel-mouse-face
+'((t (:background "peachpuff" :foreground "black")))
+  "Face for mouseovered CamelCase links.")
+
+(defvar camel-mouse-face 'camel-mouse-face)
 
 (defvar camel-map nil)
 (when (null camel-map)
@@ -81,12 +92,26 @@
 
 (defun camel-disable ()
   (camel-do-font-lock 'font-lock-remove-keywords)
-  ;; (remove-text-properties (point-min) (point-max) '(camel-fontified))
+  (remove-text-properties (point-min) (point-max) '(camel-fontified))
   (font-lock-fontify-buffer))
 
+(defvar *camel-mouse-map* (make-sparse-keymap))
+
+(define-key *camel-mouse-map* [mouse-1] 'camel-mouse-follow-link)
+
+(defun camel-render-link (begin end)
+  (add-text-properties begin end `(camel-fontified ,(buffer-substring-no-properties begin end)
+				   pointer hand
+				   keymap ,*camel-mouse-map*
+				   mouse-face camel-mouse-face
+				   help-echo "Click to visit this wiki page.")))
+				 
 (defun camel-do-font-lock (add-or-remove)
   (funcall add-or-remove nil
-	   `((,*camel-regexp* 0 ,camel-face prepend))))
+	   `((,*camel-regexp* 0 (prog1 camel-face
+				  (camel-render-link (match-beginning 0)
+						     (match-end 0)))
+			      prepend))))
 
 (provide 'camel)
 ;;; camel.el ends here
